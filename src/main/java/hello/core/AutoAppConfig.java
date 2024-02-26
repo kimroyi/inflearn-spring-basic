25a4f74cd4fe80a4c6948b2918602f49a512e23a
Royi Kim <hellowol@naver.com>
2024-02-26 23:10:48 +0900

    섹션6 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,6 +9,12 @@ import org.springframework.context.annotation.FilterType;
 //@Component 애노테이션이 붙은 클래스들을 다 스프링 빈으로 등록해준다.
 //기존의 AppConfig와는 다르게 @Bean으로 등록한 클래스가 하나도 없다!
 @ComponentScan(
+        //탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두 탐색한다.
+        //member만 컴포넌트 대상이 된다.
+        //basePackages = {"hello.core", "hello.service"} 여러 시작 위치를 지정 가능
+        //만약 basePackage를 지정하지 않으면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class, //지정한 클래스의 패키지를 탐색 시작 위치로 지정한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
