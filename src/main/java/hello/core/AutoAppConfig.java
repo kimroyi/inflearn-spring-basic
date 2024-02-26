b6a8ba533dd3efbc4a472a5a6dcd4c59ff81ee35
Royi Kim <hellowol@naver.com>
2024-02-26 23:34:43 +0900

    섹션6 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -18,5 +21,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    //수동 빈, 자동 빈 중복 등록 시 수동 빈이 우선권을 가진다. (수동 빈이 자동 빈을 오버라이딩 해버린다.)
+    //최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꿨다.
+    //허용할 경우 application.properties에 설정 가능. spring.main.allow-bean-definition-overriding=true
+    //테스트: 주석 풀고 AutoAppConfigTest 실행 -> 정상, CoreApplication 스프링부트 실행 -> 오류발생
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
